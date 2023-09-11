686b3755c78c1f75cba7b6a7830650e40d613ca9
Maciker <ikermacaya@gmail.com>
2023-09-11 22:55:13 +0200

    Complex state - Object

--- 01_part/src/App.js
+++ 01_part/src/App.js
@@ -1,6 +1,27 @@
 import { useState } from "react";
 const App = () => {
-    const [left, setLeft] = useState(0)
+    const [clicks, setClicks] = useState({
+        left: 0, right: 0
+    })
+
+    const handleLeftClick = () => {
+        setClicks({...clicks, left: clicks.left + 1})
+    }
+
+    const handleRightClick = () => {
+        setClicks({...clicks, right: clicks.right + 1})
+    }
+
+    return (
+        <div>
+            {clicks.left}
+            <button onClick={handleLeftClick}>LEFT</button>
+            <button onClick={handleRightClick}>RIGHT</button>
+            {clicks.right}
+        </div>
+    )
+
+/*    const [left, setLeft] = useState(0)
     const [right, setRight] = useState(0)
 
     return(
@@ -14,7 +35,7 @@ const App = () => {
                 RIGHT
             </button>
         </div>
-    )
+    )*/
 }
 
 export default App;
