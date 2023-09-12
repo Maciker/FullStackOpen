a4723a4951ba48d275868dee5824551af0ec3a03
Maciker <ikermacaya@gmail.com>
2023-09-12 10:27:00 +0200

    Part 1.D Exercise 1.6 - Title component

--- 01_part/src/App.js
+++ 01_part/src/App.js
@@ -1,8 +1,17 @@
 import {useState} from "react";
 
+const Title = ({title}) => {
+    return (
+        <h1>
+            {title}
+        </h1>
+    )
+}
+
 const App = () => {
     return (
         <div>
+            <Title title='Give Feedback'/>
             Hello Mac
         </div>
     )
