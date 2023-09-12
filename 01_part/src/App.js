2469685cedad46a65398ff19db20f96800c10234
Maciker <ikermacaya@gmail.com>
2023-09-12 13:15:57 +0200

    Part 1.D Exercise 1.6 - Add good feedback button and behavior

--- 01_part/src/App.js
+++ 01_part/src/App.js
@@ -8,14 +8,29 @@ const Title = ({title}) => {
     )
 }
 
+const FeedbackButton = ({handleClick, buttonText}) => {
+    return (
+        <button onClick={handleClick}>
+            {buttonText}
+        </button>
+    )
+}
+
 const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
 
+    const goodFeedbackClick = () => {
+        return setGood(good + 1)
+    }
+
     return (
         <div>
             <Title title='Give Feedback'/>
+            <br/>
+            <FeedbackButton handleClick={goodFeedbackClick} buttonText='GOOD' />
+            {good}
         </div>
     )
 }
