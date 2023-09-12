f230cf2e95823c5660ed031d36316efef8858495
Maciker <ikermacaya@gmail.com>
2023-09-12 09:28:40 +0200

    Par 1.D Handling arrays

--- 01_part/src/App.js
+++ 01_part/src/App.js
@@ -1,16 +1,38 @@
 import { useState } from "react";
+
 const App = () => {
-    const [clicks, setClicks] = useState({
-        left: 0, right: 0
-    })
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
 
     const handleLeftClick = () => {
-        setClicks({...clicks, left: clicks.left + 1})
+        setAll(allClicks.concat('L'))
+        setLeft(left + 1)
     }
 
     const handleRightClick = () => {
-        setClicks({...clicks, right: clicks.right + 1})
+        setAll(allClicks.concat('R'))
+        setRight(right + 1)
     }
+
+    return(
+        <div>
+            {left}
+            <button onClick={handleLeftClick}>
+                LEFT
+            </button>
+            {right}
+            <button onClick={handleRightClick}>
+                RIGHT
+            </button>
+            <p>{allClicks.join(' ')}</p>
+        </div>
+    )
+
+    /*
+    const [clicks, setClicks] = useState({
+        left: 0, right: 0
+    })
 
     return (
         <div>
@@ -18,22 +40,6 @@ const App = () => {
             <button onClick={handleLeftClick}>LEFT</button>
             <button onClick={handleRightClick}>RIGHT</button>
             {clicks.right}
-        </div>
-    )
-
-/*    const [left, setLeft] = useState(0)
-    const [right, setRight] = useState(0)
-
-    return(
-        <div>
-            {left}
-            <button onClick={() => setLeft(left + 1)}>
-                LEFT
-            </button>
-            {right}
-            <button onClick={() => setRight(right + 1)}>
-                RIGHT
-            </button>
         </div>
     )*/
 }
