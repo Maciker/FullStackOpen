e27ef6c84e2f5424072a9a512a120366f568c786
Maciker <ikermacaya@gmail.com>
2023-09-12 10:00:22 +0200

    Part 1.D Conditional rendering - Button component

--- 01_part/src/App.js
+++ 01_part/src/App.js
@@ -15,38 +15,33 @@ const History = (props) => {
     )
 }
 
+const Button = ({handleClick, text}) => (
+    <button onClick={handleClick}>
+        {text}
+    </button>
+)
+
 const App = () => {
     const [left, setLeft] = useState(0)
     const [right, setRight] = useState(0)
     const [allClicks, setAll] = useState([])
-    const [total, setTotal] = useState(0)
 
     const handleLeftClick = () => {
         setAll(allClicks.concat('L'))
-        const updatedLeft = left + 1
-        setLeft(updatedLeft)
-        setTotal(updatedLeft + right)
+        setLeft(left + 1)
     }
 
     const handleRightClick = () => {
         setAll(allClicks.concat('R'))
-        const updatedRight = right + 1
-        setRight(updatedRight)
-        setTotal(left + updatedRight)
+        setRight(right + 1)
     }
 
     return(
         <div>
             {left}
-            <button onClick={handleLeftClick}>
-                LEFT
-            </button>
+            <Button handleClick={handleLeftClick} text='LEFT' />
+            <Button handleClick={handleRightClick} text='RIGHT'/>
             {right}
-            <button onClick={handleRightClick}>
-                RIGHT
-            </button>
-            <p>{allClicks.join(' ')}</p>
-            <p>Total: {total}</p>
             <History allClicks={allClicks} />
         </div>
     )
