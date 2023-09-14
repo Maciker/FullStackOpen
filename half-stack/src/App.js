0a9d78c27e014b79014ca26592ab0c65d7f1edaa
Maciker <ikermacaya@gmail.com>
2023-09-14 10:35:51 +0200

    Part 2.0 Preparing data en files for exercises 2.1 - 2.5

--- half-stack/src/App.js
+++ half-stack/src/App.js
@@ -1,21 +1,43 @@
 const App = () => {
-  const course = "Half Stack App development";
-  const exercises = [
-    {
-      name: "Fundamentals of react",
-      number: 10,
-    },
-    {
-      name: "Ussing props to pass data",
-      number: 7,
-    },
-    {
-      name: "State of a component",
-      number: 14,
-    },
-  ];
+  const course = [{
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  },
+  {
+    name: 'Node.js',
+    id: 2,
+    parts: [
+      {
+        name: 'Routing',
+        exercises: 3,
+        id: 1
+      },
+      {
+        name: 'Middlewares',
+        exercises: 7,
+        id: 2
+      }
+    ]
+  }]
 
-  const Header = (props) => {
+  /*const Header = (props) => {
     return <h1>{props.course}</h1>;
   };
 
@@ -37,11 +59,12 @@ const App = () => {
         <p>Number of exercises {props.total}</p>
       </div>
     );
-  };
+  };*/
 
   
   return (
-    <>
+      <div>Exercises 2.1 --2.5</div>
+/*    <>
       <Header course={course} />
       {exercises.map((exercise) => {
         return (
@@ -52,7 +75,7 @@ const App = () => {
         );
       })}
       <Total total={totalExercises} />
-    </>
+    </>*/
   );
 };
 
