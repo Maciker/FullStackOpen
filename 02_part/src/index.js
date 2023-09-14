c738026d44c600006a9320fb29eef94f1438ce41
Maciker <ikermacaya@gmail.com>
2023-09-14 09:23:04 +0200

    Part 2.0 - Creating the App and index files

---
+++ 02_part/src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+
+const notes = [
+    {
+        id: 1,
+        content: 'Learn React',
+        important: true
+    },
+    {
+        id: 2,
+        content: 'Learn Python',
+        important: false
+    },
+    {
+        id: 1,
+        content: 'Learn Node',
+        important: true
+    }
+]
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <App notes={notes}/>
+);
