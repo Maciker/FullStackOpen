41a012e7bea8761f081603f90466b292007b6f5b
Maciker <ikermacaya@gmail.com>
2023-09-15 10:33:08 +0200

    Part 2.0 Exercises 2.1 - 2.5 Using reduce to set the total o exercises by course

--- half-stack/src/App.js
+++ half-stack/src/App.js
@@ -38,9 +38,11 @@ const App = () => {
   }]
 
   const Course = ({course}) => {
+    const totalExercises = course.parts.reduce((total, part) => total + part.exercises, 0)
     return <>
       <Header name={course.name} />
       {course.parts.map(part => <Content part={part}/>)}
+      {totalExercises}
       </>
   }
 
