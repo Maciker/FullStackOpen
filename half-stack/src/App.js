014a6ffbec206e327318f9f12f8d77f7072217d3
Maciker <ikermacaya@gmail.com>
2023-09-22 10:24:15 +0200

    Part 2.b Forms - Adding button to filter courses with exercises or not

--- half-stack/src/App.js
+++ half-stack/src/App.js
@@ -39,6 +39,11 @@ const App = () => {
     ]
   }])
   const [newCourse, setNewCourse] = useState('a new course')
+  const [showAll, setShowAll] = useState(true)
+
+  const coursesToShow = showAll
+      ? courses
+      : courses.filter( course => course.parts.reduce((total, part) => total + part.exercises, 0) > 0)
 
   const addCourse = (event) => {
     event.preventDefault()
@@ -61,7 +66,12 @@ const App = () => {
       <>
         <div>Exercises 2.1 --2.5 and 2B part: Forms</div>
         <div>
-          {courses.map(course => <Course course={course} key={course.id}/>)}
+          <button onClick={() => setShowAll(!showAll)}>
+            Show {showAll ? 'Courses with exercises' : 'All courses'}
+          </button>
+        </div>
+        <div>
+          {coursesToShow.map(course => <Course course={course} key={course.id}/>)}
         </div>
         <form onSubmit={addCourse}>
           <input value={newCourse} onChange={handleCourseChange}/>
