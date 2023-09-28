341f3db2765ea10effe9dc33a0b37a8b2ba05513
Maciker <ikermacaya@gmail.com>
2023-09-28 12:42:23 +0200

    Part 2.C adding the toggle button to set the course completed

--- half-stack/src/components/Course.js
+++ half-stack/src/components/Course.js
@@ -1,13 +1,18 @@
 import Header from './Header'
 import Content from './Content'
 import ExercisesByCourse from './ExercisesByCourse'
-const Course = ({course}) => {
+const Course = ({course, toggleFinished}) => {
     const totalExercises = course.parts.reduce((total, part) => total + part.exercises, 0)
+    const labelFinished = course.finished ? 'Course Finished' : 'Course not completed'
+    const courseFinished = course.finished ? 'Not completed' : 'Completed'
     return <>
         <Header name={course.name} />
         {course.parts.map(part => <Content part={part} key={part.id}/>)}
         <ExercisesByCourse totalExercises={totalExercises} />
-        <p>{course.finished ? 'Course Finished' : 'Course Not Finished'}</p>
+        <div>
+            <h4>{labelFinished}</h4>
+            <button onClick={toggleFinished}>{courseFinished}</button>
+        </div>
     </>
 }
 export default Course
