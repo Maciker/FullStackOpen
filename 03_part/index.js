d320d37d835f73ec91d5b52e40df414ba856a286
Maciker <ikermacaya@gmail.com>
2023-10-03 14:30:58 +0200

    Part 3.0 Installing express

--- 03_part/index.js
+++ 03_part/index.js
@@ -1,8 +1,54 @@
 const http = require('http')
 
+let courses = [
+    {
+        name: 'Half Stack application development',
+        id: 1,
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10,
+                id: 1
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7,
+                id: 2
+            },
+            {
+                name: 'State of a component',
+                exercises: 14,
+                id: 3
+            },
+            {
+                name: 'Redux',
+                exercises: 11,
+                id: 4
+            }
+        ]
+    },
+    {
+        name: 'Node.js',
+        id: 2,
+        parts: [
+            {
+                name: 'Routing',
+                exercises: 3,
+                id: 1
+            },
+            {
+                name: 'Middlewares',
+                exercises: 7,
+                id: 2
+            }
+        ]
+    }
+]
+
+
 const app = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/plain'})
-    response.end('Hello World!!')
+    response.writeHead(200, {'Content-Type': 'application/json'})
+    response.end(JSON.stringify(courses))
 })
 
 const PORT = 3001
