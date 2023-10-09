457b98aef7476b42c00179bf9e7038f7f232f521
Maciker <ikermacaya@gmail.com>
2023-10-09 12:34:45 +0200

    Part 3.0 The Phonebook API - Adding get info of one person

--- 03_part/phonebookApi/index.js
+++ 03_part/phonebookApi/index.js
@@ -38,6 +38,17 @@ app.get('/info', (request, response) => {
     response.send(`<h3> The phone Book has info for ${persons.length} people`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 const PORT = 6001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
