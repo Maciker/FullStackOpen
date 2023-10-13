1caeb004d68081e80adbec428c35815107af5d9b
Maciker <ikermacaya@gmail.com>
2023-10-13 10:31:09 +0200

    Installing and setting cors

--- 03_part/index.js
+++ 03_part/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors)
 app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
