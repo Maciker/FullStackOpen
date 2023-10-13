25a0c9117028c005dcdd2cc9b0a12cf56fc964c8
Maciker <ikermacaya@gmail.com>
2023-10-13 14:02:40 +0200

    3.0 PhoneBook: installing mongoose

--- half-stack/src/services/courses.js
+++ half-stack/src/services/courses.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = 'http://localhost:3001/courses'
+const baseUrl = 'http://localhost:3001/api/courses'
 
 const getAllCourses = () => {
     const coursesRequest= axios.get(baseUrl)
--- phonebookApi/index.js
+++ phonebookApi/index.js
@@ -3,7 +3,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
-app.use(cors)
+app.use(cors())
 app.use(express.json())
 morgan.token('body', (request, response) => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
